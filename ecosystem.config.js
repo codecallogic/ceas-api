e68e4e5f2b8a43351266122230c012ca96e6d0b3
Fabricio <contact@fabricioguardia.com>
2024-10-22 18:07:33 -0700

    pm2 config update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: "client",
+    name   : "server",
     script: 'npm start'
   }],
 
@@ -9,13 +9,13 @@ module.exports = {
       key  : 'Catsus.pem',
       user : 'ubuntu',
       host : '54.160.180.60',
-      ref  : 'origin/main',
-      repo : 'git@github.com:codecallogic/StaffAllocation.git',
-      path : '/home/ubuntu/client',
+      ref  : 'origin/master',
+      repo : 'git@github.com:codecallogic/ceas-api.git',
+      path : '/home/ubuntu/server',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
   }
-}
+};
